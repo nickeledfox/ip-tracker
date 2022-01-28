919f2a4b08fede5962c4e8f0c64548a71b0c1678
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2022-01-27 22:13:55 -0800

    Main config

---
+++ src/components/ResultsForm.vue
@@ -0,0 +1,93 @@
+<template lang="pug">
+.ResultsForm__container
+  .ResultsForm__item
+    h4.ResultsForm__item__title IP Address
+    p.ResultsForm__item__data {{IP.address}}
+
+  .ResultsForm__item
+    h4.ResultsForm__item__title Location
+    p.ResultsForm__item__data {{IP.country + ','}} {{IP.state}}
+
+  .ResultsForm__item
+    h4.ResultsForm__item__title Timezone
+    p.ResultsForm__item__data {{IP.timezone}} 
+
+  .ResultsForm__item
+    h4.ResultsForm__item__title Isp
+    p.ResultsForm__item__data {{IP.isp}}
+</template>
+
+<script>
+export default {
+  name: 'ResultForm',
+  props: ['IP'],
+};
+</script>
+
+<style scoped lang="scss">
+@use '../assets/styles/global' as *;
+.ResultsForm__container {
+  @include flex-vertical;
+  padding: 1.5rem 0;
+  background: #fff;
+  border-radius: 12px;
+  display: flex;
+  justify-content: space-around;
+  margin: 0 1rem;
+  max-width: 95vw;
+  box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;
+  z-index: 3;
+  width: 70vw;
+
+  @media (min-width: 992px) {
+    max-width: 80vw;
+  }
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+}
+.ResultsForm__item {
+  position: relative;
+  flex: 1 1 0;
+  min-width: 0;
+  font-size: 2rem;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    padding: 1rem 5rem 1rem 3rem;
+    text-align: left;
+  }
+
+  @media (min-width: 768px) {
+    &:not(:first-child) {
+      &::before {
+        content: '';
+        position: absolute;
+        left: 0;
+        bottom: 20%;
+        height: 70%;
+        border-right: 1px solid;
+        opacity: 0.2;
+      }
+    }
+  }
+
+  &__title {
+    text-transform: uppercase;
+    opacity: 0.6;
+    font-weight: 900;
+    font-size: 1.1rem;
+    letter-spacing: 1px;
+    margin: 0;
+  }
+  &__data {
+    font-weight: 700;
+    color: #000;
+
+    @media (max-width: 768px) {
+      margin: 1rem;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,119 @@
-<template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+header.header
+  h1.header__title IP Address Tracker
+  form.SearchForm(action="#")
+    input.SearchForm__input(
+      type="text",
+      placeholder="Search for any IP address or domain",
+      name="search",
+      tabindex="0",
+      v-model="queryIP"
+    )
+    button.SearchForm__btn(type="submit", @click="getIpData")
+      img(src="../assets/icons/icon-arrow.svg")
+
+  ResultsForm(:IP="IP")
+Map
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ResultsForm from '@/components/ResultsForm.vue';
+import Map from '@/components/Map.vue';
+
+import { ref } from '@vue/runtime-core';
+import axios from 'axios';
+import 'dotenv/config';
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld,
+    ResultsForm,
+    Map,
+  },
+  props: ['IP', 'queryIP', 'getIpData'],
+  setup() {
+    const queryIP = ref('');
+    let IP = ref('');
+    const KEY = process.env.VUE_APP_GEOLOCATION_API_KEY;
+    const GEOLOCATION = 'https://geo.ipify.org/api/v2/country';
+
+    const getIpData = async () => {
+      try {
+        const IPdata = await axios.get(
+          `${GEOLOCATION}?apiKey=${KEY}&ipAddress=${queryIP.value}`
+        );
+        const result = IPdata.data;
+        IP.value = {
+          address: result.ip,
+          country: result.location.country,
+          state: result.location.region,
+          timezone: result.location.timezone,
+          isp: result.isp,
+          lat: result.location.lat,
+          lng: result.location.lng,
+        };
+        console.log(result);
+      } catch (err) {
+        alert(err.message);
+      }
+    };
+    return { queryIP, IP, getIpData };
+
+    if (IP.value == '' && IP.value != null) {
+      getIpData().load();
+    }
   },
 };
 </script>
+
+<style lang="scss">
+@use '../assets/styles/global' as *;
+
+.header {
+  @include flex-vertical;
+  position: relative;
+  background-image: url('../assets/images/pattern-bg.png');
+  background-position: center;
+  background-size: cover;
+  height: 30vh;
+  padding-top: 0.5rem;
+
+  &__title {
+    color: #fff;
+    padding-bottom: 1rem;
+    letter-spacing: 0.7px;
+  }
+}
+
+.SearchForm {
+  z-index: 5;
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  margin-bottom: 3rem;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+
+  &__input {
+    padding: 1rem 2rem;
+    border: none;
+    border-radius: 12px 0 0 12px !important;
+    font-size: 2rem;
+    width: 70vw;
+    max-width: 450px;
+    transition: color linear 0.3s;
+    caret-color: #4d4d4d7e;
+  }
+  &__btn {
+    background: #000;
+    padding: 1.5rem 2.5rem;
+    border-radius: 0 12px 12px 0;
+    cursor: pointer;
+
+    @media (any-hover: hover) {
+      &:hover {
+        background: #4d4d4d;
+      }
+    }
+  }
+}
+</style>
